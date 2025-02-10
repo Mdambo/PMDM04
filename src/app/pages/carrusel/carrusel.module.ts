0308b884800cabed5e2ebf0bcee8c70c999fd816
Mdambo <152434281+Mdambo@users.noreply.github.com>
2025-02-10 12:34:56 +0100

    commit carpeta src

---
+++ src/app/pages/carrusel/carrusel.module.ts
@@ -0,0 +1,21 @@
+import { MisComponentesModule } from './../../components/mis-componentes.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CarruselPageRoutingModule } from './carrusel-routing.module';
+import { CarruselPage } from './carrusel.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CarruselPageRoutingModule,
+    MisComponentesModule,
+  ],
+  declarations: [CarruselPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CarruselPageModule {}
